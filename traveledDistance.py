dcd5e4c0a4f9960ef5bd91e86b022845cf8c5e2e
schillingderek <35021139+schillingderek@users.noreply.github.com>
2018-03-19 15:04:19 -0500

    Update traveledDistance.py

--- traveledDistance.py
+++ traveledDistance.py
@@ -5,16 +5,18 @@ def traveledDistance(travelLog):
     
     dist = 0
     
-    maps_key = 'enterKeyHere'
+    maps_key = 'enterKeyHere' #enter API key
     base_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
     
 
     start = travelLog[:-1]
     end = travelLog[1:]
+    #find all of the start and end points in the journey by zipping together an offset version of the travelLog
     travelLog = zip(start,end)
-    for d in travelLog:
-
-
+    #iterate over the travelLog and find the distance between each tuple of locations in google's distanceMatrix API
+    #could have used the full list with one call, but this produces distances bewteen every permutation of entries
+    #which seemed unnecessary and time consuming
+    for d in travelLog: 
         params = urllib.urlencode({
             'origins': d[0],
             'destinations': d[1],
@@ -23,5 +25,6 @@ def traveledDistance(travelLog):
         url = base_url + params
         response = str(urllib2.urlopen(url).read())
         map_dict = json.loads(response.replace('\\n', ''))
+        #extract the distance value from the json response of the API and add to the running distance total
         dist += (map_dict['rows'][0]['elements'][0]['distance']['value'])
     return dist
